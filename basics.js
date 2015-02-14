9b58a3e8255646bbd7ec21c71c07c051b27483d9
JessRudder <thejessicarudder@gmail.com>
2015-02-14 12:49:58 -0500

    Create repo and start basic file

---
+++ basics.js
@@ -0,0 +1,18 @@
+// Two slashes start single-line comments
+
+var x;  // declaring a variable
+
+x = 3 + y;  // assigning a value to the variable `x`
+
+foo(x, y);  // calling function `foo` with parameters `x` and `y`
+obj.bar(3);  // calling method `bar` of object `obj`
+
+// A conditional statement
+if (x === 0) {  // Is `x` equal to zero?
+    x = 123;
+}
+
+// Defining function `baz` with parameters `a` and `b`
+function baz(a, b) {
+    return a + b;
+}
