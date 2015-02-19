243e815acc9741df40129cf6ee46308bc08a0fef
JessRudder <thejessicarudder@gmail.com>
2015-02-18 20:20:53 -0500

    Array methods

--- basics.js
+++ basics.js
@@ -216,3 +216,38 @@ p.x
 //3
 p.dist()
 //5.830951894845301
+
+//Create arrays with array literals
+var arr = [ 'a', 'b', 'c' ];
+
+//Array methods
+arr.slice(1, 2)  // copy elements
+//[ 'b' ]
+arr.slice(1)
+//[ 'b', 'c' ]
+arr.push('x')  // append an element
+//4
+arr
+//[ 'a', 'b', 'c', 'x' ]
+arr.pop()  // remove last element
+//'x'
+arr
+//[ 'a', 'b', 'c' ]
+arr.shift()  // remove first element
+//'a'
+arr
+//[ 'b', 'c' ]
+arr.unshift('x')  // prepend an element
+//3
+arr
+//[ 'x', 'b', 'c' ]
+arr.indexOf('b')  // find the index of an element
+//1
+arr.indexOf('y')
+//-1
+arr.join('-')  // all elements in a single string
+//'x-b-c'
+arr.join('')
+//'xbc'
+arr.join()
+//'x,b,c'
